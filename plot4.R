7762a818859d8758221cdff5f92d16b53ecd4aba
Stanley Ku <mr.stanleyku@gmail.com>
2015-05-09 22:59:15 -0700

    finish project 1

--- plot4.R
+++ plot4.R
@@ -6,3 +6,23 @@ df$DateTime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
 
 subset <- subset(df, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
 
+png(filename="/Users/stanleyku/Desktop/ExData_Plotting1/plot4.png")
+par(mfrow = c(2,2))
+plot(subset$DateTime, subset$Global_active_power, type="l", 
+     xlab="", ylab="Global Active Power")
+
+plot(subset$DateTime, subset$Voltage, type="l",
+     xlab="datetime", ylab="Voltage")
+
+plot(subset$DateTime, subset$Sub_metering_1, type="l", 
+     ylab="Energy sub metering",
+     xlab="")
+lines(subset$DateTime, subset$Sub_metering_2, col="red")
+lines(subset$DateTime, subset$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col=c("black", "red", "blue"),
+       lty=1)
+
+plot(subset$DateTime, subset$Global_reactive_power, type="l",
+     xlab="datetime", ylab="GLobal Reactive Power")
+dev.off()
